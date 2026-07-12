3fcf28206d1d80f5f67e875038491d3012135768
choom <chaosheat12@gmail.com>
2026-07-13 01:09:27 +0900

    Update Park A Car
    Update Auto Farm

--- places/113720203996283.lua
+++ places/113720203996283.lua
@@ -1,13 +1,34 @@
 return function()
     local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/choy0on/Solaris/main/Source.lua"))()
 
+    local plr = game.Players.LocalPlayer
+    local char = plr.Character or plr.CharacterAdded:Wait()
+
     local window = SolarisLib:New({
         Name = "Titanium",
         FolderToSave = "Titanium_Settings"
     })
 
     local mainTab = window:Tab("Main")
-    local mainSection = mainTab:Section("Cars")
+
+    local FarmSection = mainTab:Section("Farm")
+
+    FarmSection:Toggle("Auto Farm", false, "Farm", function(t)
+        if t then
+            getgenv().AutoFarm = true
+            while getgenv().AutoFarm do
+                task.wait()
+                pcall(function()
+                    char:Goto()
+                end)
+            end
+        else
+            getgenv().AutoFarm = false
+        end
+        
+    end)
+
+    local CarSection = mainTab:Section("Cars")
 
     local carLabelList = {}
 
@@ -17,9 +38,9 @@ return function()
                 for i,v in pairs(workspace:FindFirstChild("SpawnedCars"):GetChildren()) do
                     if v:FindFirstChild("nameEffect") then
                         if carLabelList[i] then
-                            carLabelList[i]:Set(v.nameEffect.PartName.NameLabel.Text .. "|" .. v.nameEffect.Rarity.Text .. " | " .. v.nameEffect.Cost.Text)
+                            carLabelList[i]:Set(v.nameEffect.PartName.NameLabel.Text .. " | " .. v.nameEffect.Rarity.Text .. " | " .. v.nameEffect.Cost.Text)
                         else
-                            local newLabel = mainSection:Label(v.nameEffect.PartName.NameLabel.Text .. "|" .. v.nameEffect.Rarity.Text .. " | " .. v.nameEffect.Cost.Text)
+                            local newLabel = CarSection:Label(v.nameEffect.PartName.NameLabel.Text .. " | " .. v.nameEffect.Rarity.Text .. " | " .. v.nameEffect.Cost.Text)
                             table.insert(carLabelList, newLabel)
                         end
                     end

82be67992950bd522a795f9bf79b69fc267fef4b
choom <chaosheat12@gmail.com>
2026-07-13 04:16:17 +0900

    Update Park A Car
    List Cars Fixs

--- places/113720203996283.lua
+++ places/113720203996283.lua
@@ -5,6 +5,13 @@ return function()
     local char = plr.Character or plr.CharacterAdded:Wait()
     local hum = char:WaitForChild("Humanoid")
 
+    local RarityIcons = {
+        ["Hacker"] = "🦕",
+        ["Exclusive"] = "💎",
+        ["Secret"] = "👑",
+        ["Godly"] = "✨",
+    }
+
     local window = SolarisLib:New({
         Name = "Titanium",
         FolderToSave = "Titanium_Settings"
@@ -37,7 +44,26 @@ return function()
 
     end)
 
-    MainS:Toggle("Remove All Obstacles", false, "RemoveAllObstacles", function(t)
+    MainS:Toggle("Auto Collects", false, "AutoCollectCoins", function(t)
+        if t then
+            getgenv().AutoCollectCoins = true
+            while getgenv().AutoCollectCoins do
+                task.wait()
+                pcall(function()
+                    for i,v in pairs(workspace:FindFirstChild("Coins"):GetChildren()) do
+                        if v:IsA("MeshPart") then
+                            v.CFrame = char.HumanoidRootPart.CFrame
+                        end
+                    end
+                end)
+            end
+        else
+            getgenv().AutoCollectCoins = false
+        end
+
+    end)
+
+    MainS:Toggle("Remove All Obstacles (Include Humps)", false, "RemoveAllObstacles", function(t)
         if t then
             getgenv().RemoveAllObstacles = true
             while getgenv().RemoveAllObstacles do
@@ -53,11 +79,15 @@ return function()
                         if v.Name == "OBSTACLES" then
                             v:Destroy()
                         end
+
+                        if v.Name == "Humps" then
+                            v:Destroy()
+                        end
                     end
                 end)
             end
         else
-            getgenv().RemoveAllBroker = false
+            getgenv().RemoveAllObstacles = false
         end
 
     end)
@@ -67,21 +97,44 @@ return function()
     local carLabelList = {}
 
     pcall(function()
-        if workspace:FindFirstChild("SpawnedCars") then
-            while task.wait(1) do
-                for i,v in pairs(workspace:FindFirstChild("SpawnedCars"):GetChildren()) do
-                    if v:FindFirstChild("nameEffect") then
-                        if carLabelList[i] then
-                            carLabelList[i]:Set(v.nameEffect.PartName.NameLabel.Text .. " | " .. v.nameEffect.Rarity.Text .. " | " .. v.nameEffect.Cost.Text)
-                        else
-                            local newLabel = CarSection:Label(v.nameEffect.PartName.NameLabel.Text .. " | " .. v.nameEffect.Rarity.Text .. " | " .. v.nameEffect.Cost.Text)
-                            table.insert(carLabelList, newLabel)
-                        end
-                    end
-                end
-            end
-        end
-    end)
+		if workspace:FindFirstChild("SpawnedCars") then
+			workspace:FindFirstChild("SpawnedCars").ChildAdded:Connect(function(car)
+				local nameEffect = car:WaitForChild("nameEffect", 10)
+				local nameLabel = nameEffect:WaitForChild("PartName", 10).NameLabel
+				local rarityName = nameEffect:WaitForChild("Rarity", 10).Text
+				local rarityIcon = RarityIcons[rarityName]
+				local cost = nameEffect.Cost
+				local carText
+
+				if rarityIcon then
+					carText = string.format(
+						"%s | %s %s | %s",
+						nameLabel.Text,
+						rarityIcon,
+						rarityName,
+						cost.Text
+					)
+				else
+					carText = string.format(
+						"%s | %s | %s",
+						nameLabel.Text,
+						rarityName,
+						cost.Text
+					)
+				end
+
+				local butn = CarSection:Button(carText, function()
+					char:MoveTo(car.WorldPivot.Position)
+				end)
+
+				carLabelList[car] = butn
+			end)
+
+			workspace:FindFirstChild("SpawnedCars").ChildRemoved:Connect(function(car)
+				carLabelList[car]:Destroy()
+			end)
+		end
+	end)
 
     return window
 end

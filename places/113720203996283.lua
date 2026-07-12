12566c3b374f07b1dbeb8d5e7f31ff02a0101a6a
choom <chaosheat12@gmail.com>
2026-07-13 02:02:18 +0900

    Update Park A Car
    Added Function Remove All Obstacles

--- places/113720203996283.lua
+++ places/113720203996283.lua
@@ -3,6 +3,7 @@ return function()
 
     local plr = game.Players.LocalPlayer
     local char = plr.Character or plr.CharacterAdded:Wait()
+    local hum = char:WaitForChild("Humanoid")
 
     local window = SolarisLib:New({
         Name = "Titanium",
@@ -11,21 +12,48 @@ return function()
 
     local mainTab = window:Tab("Main")
 
-    local FarmSection = mainTab:Section("Farm")
+    local MainS = mainTab:Section("Main")
 
-    FarmSection:Toggle("Auto Farm", false, "Farm", function(t)
+    MainS:Toggle("Auto Farm", false, "Farm", function(t)
         if t then
             getgenv().AutoFarm = true
             while getgenv().AutoFarm do
                 task.wait()
                 pcall(function()
-                    char:Goto()
+                    local cars = {}
+
+                    for i,v in pairs(workspace:FindFirstChild("SpawnedCars"):GetChildren()) do
+                        table.insert(cars, v)
+                    end
+
+                    for i,v in pairs(cars) do
+                        
+                    end
                 end)
             end
         else
             getgenv().AutoFarm = false
         end
-        
+
+    end)
+
+    MainS:Toggle("Remove All Obstacles", false, "RemoveAllObstacles", function(t)
+        if t then
+            getgenv().RemoveAllBroker = true
+            while getgenv().RemoveAllBroker do
+                task.wait()
+                pcall(function()
+                    for i,v in pairs(workspace:FindFirstChild("Obstacles"):GetDescendants()) do
+                        if v:IsA("Model") then
+                            v:Destroy()
+                        end
+                    end
+                end)
+            end
+        else
+            getgenv().RemoveAllBroker = false
+        end
+
     end)
 
     local CarSection = mainTab:Section("Cars")
